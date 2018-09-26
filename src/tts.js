7bd119e898a1082427c9bdfc41403b4c8b4b4c0e
amfang <fangam@cn.ibm.com>
2018-09-26 15:08:35 +0800

    Update back of tts GET for 403

--- src/tts.js
+++ src/tts.js
@@ -82,7 +82,7 @@ exports.initTTS = function () {
 
       //检测输入文字语言
       var languageIdentifyCall = {
-        type: 'GET',
+        type: 'POST',
         url: '/api/identify',
         headers: {
           'X-Watson-Technology-Preview': nmtValue
